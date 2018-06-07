9b797531ec5eb4feb81bb2624ba524669bd337a0
Administrator <616928939@qq.com>
2018-06-07 16:05:55 +0800

    舒适

--- src/main/java/com/example/demo/Demo01Application.java
+++ src/main/java/com/example/demo/Demo01Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo01Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
 	}
